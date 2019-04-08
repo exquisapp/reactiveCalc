7061112060bf3bbed22982f51ec7b776d387a28b
Jurshsmith <jurshsmith@gmail.com>
2019-04-08 08:46:39 +0100

    calculator functionality complete

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Pizza } from './../models/pizza.model';
 import * as fromStore from './../store';
+import { CalculatorState } from './../store/reducers/calculator.reducer';
 
 import * as allActions from './../store/actions/calculator.action';
 
@@ -14,14 +15,33 @@ import * as allActions from './../store/actions/calculator.action';
 })
 export class CalculatorComponent implements OnInit {
 //a dependency injection from the StoreModule
+  private calculatorState: CalculatorState;
+
   constructor(private store: Store<fromStore.ProductState>) { }
 
   ngOnInit() {
-    this.store.select<any>('reactiveCalc').subscribe(data => console.log(data));
+    this.store.select<any>('reactiveCalc').subscribe(data => this.calculatorState = data);
   }
 
   dispatchLoad(){
     this.store.dispatch(new allActions.Add(2));
+  }
+
+  dispatchNumberClicked(number: number){
+    this.store.dispatch(new allActions.NumberEntered(number));
+  }
+
+  dispatchOp(op: string){
+  
+    this.store.dispatch(new allActions.OpEntered(op));
+  }
+
+  getTotal(){
+    this.store.dispatch(new allActions.GetTotal());
+  }
+
+  clear(){
+    this.store.dispatch(new allActions.Clear());
   }
 
 }
--- src/app/store/actions/calculator.action.ts
+++ src/app/store/actions/calculator.action.ts
@@ -5,6 +5,11 @@ export const ADD = "[Calc] add operation";
 export const SUBTRACT = "[Calc] subtract operation";
 export const DIVISION = "[Calc] DIVISION operation";
 export const MULTIPLICATION = "[Calc] MULTIPLICATION operation";
+export const NUMBER_ENTERED = "[Calc] number entered";
+export const OP_ENTERED = "[Calc] OP entered";
+export const GET_TOTAL = "[Calc] Get total entered";
+export const CLEAR = "[CALC] CLEAR";
+
 
 export class Add implements Action{
     readonly type = ADD;
@@ -26,4 +31,22 @@ export class Multiplication implements Action {
     constructor(public payload: number){}
 }
 
-export type calculatorTypes = Add | Subtract | Division | Multiplication;
+export class NumberEntered implements Action {
+    readonly type = NUMBER_ENTERED;
+    constructor(public payload: number){}
+}
+
+export class OpEntered implements Action {
+    readonly type = OP_ENTERED;
+    constructor(public payload: string){}
+}
+
+export class GetTotal implements Action {
+    readonly type = GET_TOTAL;
+}
+
+export class Clear implements Action {
+    readonly type = CLEAR;
+}
+
+export type calculatorTypes = Add | Subtract | Division | Multiplication | NumberEntered | OpEntered | GetTotal | Clear;
